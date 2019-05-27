cd78d991762372fb161c882b1befcc7d8a00526f
Faizan Aamer <faizanaryan94@gmail.com>
2019-05-28 02:01:06 +0500

    Api keys removed

--- ConnectApi/AppProperties/AppConstants.cs
+++ ConnectApi/AppProperties/AppConstants.cs
@@ -25,12 +25,12 @@ namespace ConnectApi.AppProperties
 
 
         //One API Key and AppId
-        public const string OneSignalAppId = "3399eeba-1516-4e78-8867-d2eee5a0a4ed";
-        public const string OneSignalApiKey = "YWNmNGQwMjMtODQ1MC00MDUwLWExNWYtYTZhMThkOTNhYWNm";
+        public const string OneSignalAppId = "";
+        public const string OneSignalApiKey = "";
 
-        public const string TwilioAccountSid = "AC987493e37ffee3357e84002d6920d119"; //"ACa02f9462920d0d1bb1c7f90552dfbf5b";
-        public const string TwilioAuthToken = "75867b180dc29766cd7b9ea6cb543833";//"23b62f41a2ad00ffbc64e1f345ae6b45";
-        public const string TwilioFromPhoneNumber = "+13345390248"; //"+13145820655";
+        public const string TwilioAccountSid = ""; //"";
+        public const string TwilioAuthToken = "";//"";
+        public const string TwilioFromPhoneNumber = ""; //"";
 
 
 
